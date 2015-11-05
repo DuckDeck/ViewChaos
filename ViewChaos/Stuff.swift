84b35e668a7735bed881e10f31dbbb338e496cf2
DuckDeck <3421902@qq.com>
2015-11-05 11:26:29 +0800

    Release 1.0.0.

---
+++ ViewChaos/Stuff.swift
@@ -0,0 +1,8 @@
+//
+//  Stuff.swift
+//  ViewChaosDemo
+//
+//  Created by Tyrant on 11/5/15.
+//  Copyright © 2015 Qfq. All rights reserved.
+//
+
---
+++ ViewChaos/ViewChaos.swift
@@ -0,0 +1,21 @@
+//
+//  ViewChaos.swift
+//  ViewChaosDemo
+//
+//  Created by Tyrant on 11/5/15.
+//  Copyright © 2015 Qfq. All rights reserved.
+//
+
+import UIKit
+
+class ViewChaos: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
